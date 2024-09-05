4ef72cba7ed94867f651e96a375a509efe77d6cb
Eric Bang <ericbang2000@gmail.com>
2024-09-06 00:12:24 +0900

    update widhet

--- js/widget.js
+++ js/widget.js
@@ -15,6 +15,7 @@
 	  i.style.zIndex = "2147483647";
 	  i.style.setProperty("z-index", "2147483647", "important");
 	  i.classList.add("notranslate");
+	  i.classList.add("ch-plugin-off");
 	  i.classList.add("ch-plugin-message-off");
 	  i.innerHTML +=
 		'<div id="ch-plugin-entry" style="position:relative !important"><div id="ch-plugin-launcher"><div id="ch-plugin-message-box"><div id="ch-plugin-message-button"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/></svg></div><div id="ch-plugin-message"></div></div><button type="button" id="ch-plugin-launcher-button"><div class="inner"><div id="ch-plugin-launcher-on"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF" class="w-6 h-6"><path fill-rule="evenodd" d="M5.337 21.718a6.707 6.707 0 0 1-.533-.074.75.75 0 0 1-.44-1.223 3.73 3.73 0 0 0 .814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 0 1-4.246.997Z" clip-rule="evenodd"/></svg></div><div id="ch-plugin-launcher-off"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="#555555" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/></svg></div></div></button></div></div>';
